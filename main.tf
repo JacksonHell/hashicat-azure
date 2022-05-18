f99fc3ead568d7cf24703a79d85d7b1a038356c7
JacksonHell <christopher.onate@gmail.com>
2022-05-18 17:39:36 +0000

    Added network module

--- main.tf
+++ main.tf
@@ -104,7 +104,6 @@ resource "azurerm_public_ip" "catapp-pip" {
 
 resource "azurerm_virtual_machine" "catapp" {
   name                = "${var.prefix}-meow"
-  department          = devops
   location            = var.location
   resource_group_name = azurerm_resource_group.myresourcegroup.name
   vm_size             = var.vm_size
@@ -136,7 +135,12 @@ resource "azurerm_virtual_machine" "catapp" {
     disable_password_authentication = false
   }
 
-  tags = {}
+  tags = {
+
+    department = devops 
+    billable  = true
+
+  }
 
   # Added to allow destroy to work correctly.
   depends_on = [azurerm_network_interface_security_group_association.catapp-nic-sg-ass]
